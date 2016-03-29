4f66468e2444def64dfbd957aa6c670d568bfeb3
Sergio Cabrera <sergio.cabrera@uruit.com>
2016-03-29 13:15:16 -0300

    SQLite por iOS

---
+++ Feedbapp/Feedbapp.iOS/SQLite_iOS.cs
@@ -0,0 +1,31 @@
+﻿using Feedbapp.iOS;
+using Feedbapp.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLite_iOS))]
+
+namespace Feedbapp.iOS
+{
+    public class SQLite_iOS : ISQLite
+    {
+        public SQLite_iOS()
+        {
+        }
+
+        public SQLite.SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "TodoSQLite.db3";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
+            string libraryPath = Path.Combine(documentsPath, "..", "Library"); // Library folder
+            var path = Path.Combine(libraryPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLite.SQLiteConnection(path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
